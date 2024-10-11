de9d46ed4f2446d36dd1758d1f54ce723c9c3658
Cuau Cabrera <cabrera.cuau@gmail.com>
2024-10-11 18:56:18 -0500

    Agrega descripcion de los metodos CRUD a la documentacion de OpenAPI

--- src/main/java/com/isol/usuarios/controller/UsuarioController.java
+++ src/main/java/com/isol/usuarios/controller/UsuarioController.java
@@ -5,6 +5,10 @@ import com.isol.usuarios.exceptions.ResponseNotFound;
 import com.isol.usuarios.model.ResponseSave;
 import com.isol.usuarios.model.UsuarioDTO;
 import com.isol.usuarios.service.impl.UsuarioService;
+import com.isol.usuarios.utils.UsuarioConstantes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,24 +25,51 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    @Operation(summary = UsuarioConstantes.DOC_READ_ALL_SUM, description = UsuarioConstantes.DOC_READ_ALL_DESC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = UsuarioConstantes.SUCCESS_MSG),
+            @ApiResponse(responseCode = "404",description = UsuarioConstantes.NO_CONTENT_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/usuarios")
     public List<UsuarioDTO> readAll() throws ResponseNoContent {
         return usuarioService.readAll();
     }
 
+
+    @Operation(summary =UsuarioConstantes.DOC_READ_BY_ID_SUM, description = UsuarioConstantes.DOC_READ_BY_ID_DESC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = UsuarioConstantes.SUCCESS_MSG),
+            @ApiResponse(responseCode = "404",description = UsuarioConstantes.NOT_FOUND_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/usuarios/{id}")
     public UsuarioDTO readById(@PathVariable Long id) throws ResponseNotFound {
         return usuarioService.readById(id);
     }
 
+
+    @Operation(summary = UsuarioConstantes.DOC_INSERT_SUM, description = UsuarioConstantes.DOC_INSERT_DEC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = UsuarioConstantes.CREATED_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/usuarios")
     public ResponseSave insert(@RequestBody UsuarioDTO usuarioDTO){
         return usuarioService.insert(usuarioDTO);
     }
 
+
+    @Operation(summary = UsuarioConstantes.DOC_UPDATE_SUM, description = UsuarioConstantes.DOC_UPDATE_DESC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = UsuarioConstantes.CREATED_MSG),
+            @ApiResponse(responseCode = "404",description = UsuarioConstantes.NOT_FOUND_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PutMapping("/usuarios/{id}")
     public ResponseSave update(@PathVariable("id") Long id, @RequestBody UsuarioDTO usuarioDTO) throws ResponseNotFound {
@@ -46,12 +77,26 @@ public class UsuarioController {
      return usuarioService.update(id,usuarioDTO);
     }
 
+
+    @Operation(summary = UsuarioConstantes.DOC_DELETE_SUM, description = UsuarioConstantes.DOC_DELETE_DESC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = UsuarioConstantes.DELETED_MSG),
+            @ApiResponse(responseCode = "404",description = UsuarioConstantes.NOT_FOUND_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/usuarios/{id}")
     public ResponseSave deleteById(@PathVariable("id") Long id) throws ResponseNotFound {
         return usuarioService.deleteById(id);
     }
 
+
+    @Operation(summary = UsuarioConstantes.DOC_FIND_BY_EMAIL_SUM, description = UsuarioConstantes.DOC_FIND_BY_EMAIL_DESC)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = UsuarioConstantes.SUCCESS_MSG),
+            @ApiResponse(responseCode = "404",description = UsuarioConstantes.NOT_FOUND_MSG),
+            @ApiResponse(responseCode = "500",description = UsuarioConstantes.SERVER_ERROR_MSG)
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/usuarios/email")
     public UsuarioDTO findByEmail(@RequestParam("email") String email) throws ResponseNotFound {
--- src/main/java/com/isol/usuarios/utils/UsuarioConstantes.java
+++ src/main/java/com/isol/usuarios/utils/UsuarioConstantes.java
@@ -17,5 +17,17 @@ public class UsuarioConstantes {
     public static final String REQUIRED_APELLIDO = "El campo apellido es obligatorio";
     public static final String REQUIRED_EMAIL = "El campo email es obligatorio";
     public static final String INVALID_EMAIL = "Por favor ingrese una direccion de correo valida";
+    public static final String DOC_READ_ALL_SUM = "Obtiene todos los usuarios activos";
+    public static final String DOC_READ_ALL_DESC = "Recupera una lista con todos los usuarios que cumplen la condicion isActive = true";
+    public static final String DOC_READ_BY_ID_SUM = "Obtiene registros unicos por Id";
+    public static final String DOC_READ_BY_ID_DESC="Recupera registros unicos en funcion del Id de cada usuario. El resultado es un regisro unico que cumple la condicion isActive = true";
+    public static final String DOC_INSERT_SUM = "Crea registros individuales de nuevos usuarios";
+    public static final String  DOC_INSERT_DEC = "Crea un registro nuevo de la entidad Usuario a partir de la informacion especificada en el cuerpo de la peticion. Su valor isActive por defecto es true";
+    public static final String DOC_UPDATE_SUM = "Actualiza registros individuales de usuarios existentes";
+    public static final String DOC_UPDATE_DESC = "Actualiza un registro existente de la entidad Usuario a partir de la informacion especificada en el cuerpo de la peticion. La eleccion del registro que sera actualizada se realiza a traves de la declaracion del Id como parte del PathVariable";
+    public static final String DOC_DELETE_SUM = "Desactiva registros individuales de usuarios existentes";
+    public static final String DOC_DELETE_DESC = "Realiza un borrado logico de registros individuales a traves de la definicion del Id como parte del PathVariable. La operacion logica define el valor del campo isActive=false";
+    public static final String DOC_FIND_BY_EMAIL_SUM = "Consulta registros unicos de usuarios en funcion del campo email";
+    public static final String DOC_FIND_BY_EMAIL_DESC = "Realiza una consulta de registros individuales usando como parametro de busqueda el campo email. El parametro de busqueda email es definido mediante query string. Los registros recuperados cumplen con la condicIon isActive= true";
 
 }
